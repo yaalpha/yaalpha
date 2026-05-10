9ffe8bb847e11a373c802915766ffa7fd7cb722b
yaalpha <70707470+yaalpha@users.noreply.github.com>
2026-05-10 21:54:44 +0300

    refactor: extract responsive media queries into a dedicated stylesheet

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './styles/base.css';
 import './styles/layout.css';
 import './styles/components.css';
 import './styles/animations.css';
+import './styles/responsive.css';
 
 import { initClock } from './js/clock.js';
 import { initThemeToggle } from './js/theme.js';
